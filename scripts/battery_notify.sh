e8fdd8185940ecc2fce955ad1acf104f48b14623
Niall Ryan <niallcryan@live.ie>
2024-02-08 14:51:12 +0000

    Fixed issue with battery notification script - required variables to be set.

--- scripts/battery_notify.sh
+++ scripts/battery_notify.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export DISPLAY=":0"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+
 # Get the battery path & info
 battery_path=$(upower -e | grep 'battery')
 battery_info=$(upower -i $battery_path)
@@ -7,9 +10,6 @@ battery_info=$(upower -i $battery_path)
 # Extract the percentage and state
 battery_level=$(echo "$battery_info" | grep 'percentage' | grep -o '[0-9]\+')
 battery_state=$(echo "$battery_info" | grep 'state' | awk '{print $2}')
-
-echo $battery_level
-echo $battery_state
 
 # Notification conditions
 if [[ "$battery_state" == "discharging" && "$battery_level" -le 20 ]]; then
